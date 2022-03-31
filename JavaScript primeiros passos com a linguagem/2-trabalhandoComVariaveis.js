2dc4fbcc3a92075ae2c38fc69399881e0d49fbbd
Pedro Rodrigues <101387000+phsrdev@users.noreply.github.com>
2022-03-30 23:16:57 -0300

    Alura - Front End
    Curso ministrado por Ricardo Bugan, primeiros passos em JavaScript

---
+++ JavaScript primeiros passos com a linguagem/2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Pedro";
+
+//não fazer
+//mes = "Abril";
