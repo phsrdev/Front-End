e49e9930ed95f70109b4f026295d775c3664cc9a
Pedro Rodrigues <101387000+phsrdev@users.noreply.github.com>
2022-04-02 16:17:27 -0300

    Ministrado por Ricardo Bugan

---
+++ JavaScript interfaces e Herança em Orientação a Objetos/index - Copia.js
@@ -0,0 +1,14 @@
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { ContaSalario } from "./Conta/ContaSalario.js";
+
+const cliente1 = new Cliente("Pedro", 12345678910);
+
+const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+
+console.log(contaSalario);
---
+++ JavaScript interfaces e Herança em Orientação a Objetos/index.js
@@ -0,0 +1,18 @@
+import {Cliente} from "./Cliente.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
+
+const diretor =  new Diretor("Juliana", 10000, 10987654321);
+diretor.cadastrarSenha("123456")
+const gerente =  new Gerente("Pedro",  5000, 12345678910);
+gerente.cadastrarSenha("123");
+
+const cliente = new Cliente("Rodrigo", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
